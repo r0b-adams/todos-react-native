128acae77b8025f588a486357316806c75020688
Robert Adams <adamsiii.robert@gmail.com>
2022-07-15 14:26:12 -0700

    updated imports

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet, View } from 'react-native';
 
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
