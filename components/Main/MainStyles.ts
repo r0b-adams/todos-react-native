b9618071f5c6bee24cc9e40359f450f9c1903108
Robert Adams <adamsiii.robert@gmail.com>
2022-07-15 14:27:06 -0700

    added style exports

---
+++ components/Main/MainStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const MainStyles = StyleSheet.create({
+  main: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'lightblue',
+  },
+});
+
+export default MainStyles.main;
