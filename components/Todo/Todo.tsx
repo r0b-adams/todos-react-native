69fe939e62ca790c1af81699ad988b8c7acf543c
Robert Adams <adamsiii.robert@gmail.com>
2022-07-18 11:59:20 -0700

    created Todo component

---
+++ components/Todo/Todo.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+import { Todo } from '../../@types';
+
+const Todo = () => {
+  return (
+    <View>
+      <Text>Todo</Text>
+    </View>
+  );
+};
+
+export default Todo;
