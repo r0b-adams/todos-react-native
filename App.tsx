b5391de3d84caffcb2f26e71270366f06aee9784
Robert Adams <adamsiii.robert@gmail.com>
2022-07-18 12:01:39 -0700

    added app state

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
-import { vh } from './helpers/viewportUnits';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-export default function App() {
-  return (
-    <View style={styles.app}>
-      <Header />
-      <Main />
-      <Footer />
-    </View>
-  );
-}
+import { vh } from './helpers/viewportUnits';
+import { Todos } from './@types';
 
 const styles = StyleSheet.create({
   app: {
     height: vh(100),
   },
 });
+
+export default function App() {
+  const [view, setView] = useState('Todos');
+  const [todos, setTodos] = useState<Todos>({
+    inProgress: [],
+    completed: [],
+  });
+
+  return (
+    <View style={styles.app}>
+      <Header />
+      {view === 'Todos' && <Main view={view} todos={todos.inProgress} />}
+      {view === 'Completed' && <Main view={view} todos={todos.completed} />}
+      <Footer setView={setView} />
+    </View>
+  );
+}
