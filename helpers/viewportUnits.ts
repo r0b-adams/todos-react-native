d0c366832bf8cfc7a58fa6db3b3e96d866883de4
Robert Adams <adamsiii.robert@gmail.com>
2022-07-18 11:58:30 -0700

    updated var names

--- helpers/viewportUnits.ts
+++ helpers/viewportUnits.ts
@@ -2,14 +2,14 @@ import { Dimensions } from 'react-native';
 
 const vh = (percentage: number) => {
   if (percentage < 0) throw new Error('percentage must be greater than zero');
-  const h = Dimensions.get('window').height;
-  return h * (percentage / 100);
+  const windowHeight = Dimensions.get('window').height;
+  return windowHeight * (percentage / 100);
 };
 
 const vw = (percentage: number) => {
   if (percentage < 0) throw new Error('percentage must be greater than zero');
-  const w = Dimensions.get('window').width;
-  return w * (percentage / 100);
+  const windowWidth = Dimensions.get('window').width;
+  return windowWidth * (percentage / 100);
 };
 
 export { vh, vw };
